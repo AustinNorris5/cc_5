c1ade199a37dc7bf8f7fcfb732f8d767b3c6a986
AustinNorris5 <ajnorris@usf.edu>
2025-02-05 16:30:23 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -10,3 +10,22 @@ let customer = {
 console.log(`Name: ${customer.name}`);
 console.log(`Age: ${customer.age}`);
 console.log(`Email: ${customer.email}`);
+
+//Task 2: Object Methods
+
+//Declare an object order with properties
+let order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+//Add a method displayOrder() inside the object to log the order details using this
+displayOrder() {
+    console.log(`Order ID: ${this.orderId}`);
+    console.log(`Total Amount: ${this.totalAmount}`);
+    console.log(`Status: ${this.status}`);
+}
+};
+
+//Call the metod and log the details
+order.displayOrder();
+
