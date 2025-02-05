582af7bc09bc7468d940e83745869381376bb1a5
AustinNorris5 <ajnorris@usf.edu>
2025-02-05 16:07:47 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,12 @@
+//Task 1: Object Properties
+
+//Declare an object customer with the properties
+let customer = {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+};
+//Log each property using console.log() and a template literal
+console.log(`Name: ${customer.name}`);
+console.log(`Age: ${customer.age}`);
+console.log(`Email: ${customer.email}`);
